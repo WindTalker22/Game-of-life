a6c3b554772d257104f64e19b0c0113fe3b6687e
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-06-19 19:09:48 -0400

    Created Board

---
+++ src/components/GameBoard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+const CELL_SIZE = 20
+const WIDTH = 800
+const HEIGHT = 600
+
+class GameBoard extends Component {
+  render() {
+    return (
+      <div>
+        <div
+          className='Board'
+          style={{
+            width: WIDTH,
+            height: HEIGHT,
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+          }}
+        >
+          {' '}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GameBoard
