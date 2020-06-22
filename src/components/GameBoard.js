f55c72cdc4cb33d7f67b9be3eaa30afd9d40e120
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-06-22 12:20:19 -0400

    Created grid and clickable cells

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import produce from 'immer'
 
-const CELL_SIZE = 20
-const WIDTH = 800
-const HEIGHT = 600
+const numRows = 50
+const numCols = 50
 
-class GameBoard extends Component {
-  render() {
-    return (
-      <div>
-        <div
-          className='Board'
-          style={{
-            width: WIDTH,
-            height: HEIGHT,
-            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
-          }}
-        >
-          {' '}
-        </div>
-      </div>
-    )
-  }
+const GameBoard = () => {
+  // <------Creating Grid Beginning------->
+  const [grid, setGrid] = useState(() => {
+    const rows = []
+
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+
+    return rows
+  })
+  // <-------Creating Grid End---------->
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, j) => (
+          <div
+            key={`${i}-${j}`}
+            onClick={() => {
+              const newGrid = produce(grid, (gridCopy) => {
+                gridCopy[i][j] = grid[i][j] ? 0 : 1
+              })
+              setGrid(newGrid)
+            }}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: grid[i][j] ? 'pink' : undefined,
+              border: '1px solid black',
+            }}
+          />
+        ))
+      )}
+    </div>
+  )
 }
 
 export default GameBoard
