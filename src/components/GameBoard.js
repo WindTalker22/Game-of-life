fc766b0fe3ca344db1dec61d283c8bf9bd65a9cc
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-06-22 13:09:17 -0400

    Implemented game logic

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useRef } from 'react'
 import produce from 'immer'
 
 const numRows = 50
 const numCols = 50
 
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+]
+
+generateEmptyGrid()
+
 const GameBoard = () => {
-  // <------Creating Grid Beginning------->
+  // <------Creating Grid------->
   const [grid, setGrid] = useState(() => {
     const rows = []
 
@@ -15,35 +28,83 @@ const GameBoard = () => {
 
     return rows
   })
-  // <-------Creating Grid End---------->
+
+  const [running, setRunning] = useState(false)
+
+  const runningRef = useRef(running)
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let neighbors = 0
+            operations.forEach(([x, y]) => {
+              const newI = i + x
+              const newJ = j + y
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += g[newI][newJ]
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1
+            }
+          }
+        }
+      })
+    })
+
+    setTimeout(runSimulation, 1000)
+  }, [])
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 20px)`,
-      }}
-    >
-      {grid.map((rows, i) =>
-        rows.map((col, j) => (
-          <div
-            key={`${i}-${j}`}
-            onClick={() => {
-              const newGrid = produce(grid, (gridCopy) => {
-                gridCopy[i][j] = grid[i][j] ? 0 : 1
-              })
-              setGrid(newGrid)
-            }}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: grid[i][j] ? 'pink' : undefined,
-              border: '1px solid black',
-            }}
-          />
-        ))
-      )}
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running)
+          if (!running) {
+            runningRef.current = true
+            runSimulation()
+          }
+        }}
+      >
+        {running ? 'stop' : 'start'}
+      </button>
+      <button>clear</button>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              key={`${i}-${j}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][j] = grid[i][j] ? 0 : 1
+                })
+                setGrid(newGrid)
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][j] ? 'pink' : undefined,
+                border: '1px solid black',
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
   )
 }
 
