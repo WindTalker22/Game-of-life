be0d1d37f57e1f248000386e4d33fbbd3fcb827e
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-06-24 12:45:29 -0400

    Implemented colors of cells as well as generation count and population count

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,8 +1,9 @@
 import React, { useState, useCallback, useRef } from 'react'
 import produce from 'immer'
+var _ = require('lodash')
 
 const numRows = 30
-const numCols = 40
+const numCols = 50
 
 const operations = [
   [0, 1],
@@ -14,6 +15,8 @@ const operations = [
   [1, 0],
   [-1, 0],
 ]
+
+const colors = ['#F3FE7E', '#979797', '#757575']
 
 const generateEmptyGrid = () => {
   const rows = []
@@ -43,6 +46,8 @@ const GameBoard = () => {
       return
     }
     setGrid((g) => {
+      let isGrid = false
+      isGrid = false
       return produce(g, (gridCopy) => {
         for (let i = 0; i < numRows; i++) {
           for (let j = 0; j < numCols; j++) {
@@ -59,10 +64,21 @@ const GameBoard = () => {
             if (neighbors < 2 || neighbors > 3) {
               gridCopy[i][j] = 0
             } else if (g[i][j] === 0 && neighbors === 3) {
+              isGrid = true
               gridCopy[i][j] = 1
             }
           }
         }
+
+        if (isGrid) {
+          setGeneration((num) => num + 1)
+        }
+
+        setSum(
+          gridCopy.flat().reduce((acc, cv) => {
+            return acc + cv
+          })
+        )
       })
     })
 
@@ -91,7 +107,7 @@ const GameBoard = () => {
               for (let i = 0; i < numRows; i++) {
                 rows.push(
                   Array.from(Array(numCols), () =>
-                    Math.random() > 0.5 ? 1 : 0
+                    Math.random() > 0.7 ? 1 : 0
                   )
                 )
               }
@@ -104,6 +120,8 @@ const GameBoard = () => {
           <button
             onClick={() => {
               setGrid(generateEmptyGrid())
+              setGeneration(0)
+              setSum(0)
             }}
           >
             Clear
@@ -131,6 +149,12 @@ const GameBoard = () => {
           </button>
         </div>
       </div>
+      <div className='InfoBox'>
+        <div className='Info'>
+          <p>Generation: {generation}</p>
+          <p>Population: {sum}</p>
+        </div>
+      </div>
       <div
         className='Board'
         style={{
@@ -153,8 +177,17 @@ const GameBoard = () => {
                 // living cells
                 width: 20,
                 height: 20,
-                backgroundColor: grid[i][j] ? 'black' : undefined,
-                border: '1px solid black',
+                backgroundColor:
+                  grid[i][j] && sum > 100
+                    ? _.sample(colors)
+                    : grid[i][j] && sum > 75
+                    ? '#F3FE7E'
+                    : grid[i][j] && sum > 50
+                    ? '#979797'
+                    : grid[i][j] && sum >= 0
+                    ? '#757575'
+                    : '#000000',
+                border: '1px solid #000000',
               }}
             />
           ))
