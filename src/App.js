38f27f25f965d00393933ea0e0ecc0fe1c665fd0
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-06-24 17:42:12 -0400

    Was goingto incorporate a video but decided not to

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './App.css'
 import GameBoard from './components/GameBoard'
+import Space from './video/401532737.mp4'
 
 function App() {
   return (
     <div>
+      {/* <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: 'fixed',
+          width: '100%',
+          left: '50%',
+          top: '50%',
+          height: '100vh',
+          objectFit: 'cover',
+          transform: 'translate(-50%,-50%)',
+          zIndex: '-1',
+        }}
+      >
+        <source src={Space} />
+      </video> */}
       <h1 className='Title'>Conways Game Of Life</h1>
       <GameBoard />
     </div>
